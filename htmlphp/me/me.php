8520e736094a57f394715100ac06e3a014a42a1a
Fredrik Renander <fredrikrenander@gmail.com>
2015-02-14 12:06:53 +0100

    First version

---
+++ htmlphp/me/me.php
@@ -0,0 +1,16 @@
+<?php include("incl/config.php"); ?>
+
+<?php
+$title = "Min Me-sida om mig själv";
+?>
+
+<?php include("incl/header.php"); ?>
+ 
+<!-- Sidans huvudsakliga innehåll -->
+<h1>htmlphp-me</h1>
+
+  <p>Fredriks sida</p>
+  <img src="img/me.jpg" alt="Bild på Fredrik Renander"> 
+
+<?php include("incl/footer.php"); ?>
+    
---
+++ htmlphp/me/report.php
@@ -0,0 +1,16 @@
+<?php include("incl/config.php"); ?>
+
+<?php
+$title = "Mina redovisningar av kursmomenten";
+?>
+
+<?php include("incl/header.php"); ?>
+  
+<!-- Sidans huvudsakliga innehåll -->
+<h1>Mina Redovisningar</h1>
+
+  <p>Text kommer snart</p>
+  <img src="img/me.jpg" alt="Bild på Fredrik Renander"> 
+
+<?php include("incl/footer.php"); ?>
+    
